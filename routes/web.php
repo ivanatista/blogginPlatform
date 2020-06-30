78c805e624f89b5ecba48d8f73fb259cfaa05696
Ivan Batista <ivanatista@gmail.com>
2020-06-30 05:44:43 -0400

    Post Listing

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function(){
+    $posts = \App\Posts::all();
+
+    return view('welcome', ['posts'=> $posts]);
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
