eedee2f2b769b5ef5ebe6de7a779b8e137553ae5
Ivan Batista <ivanatista@gmail.com>
2020-06-30 05:23:32 -0400

    Added Factory & Seeder for mock data

---
+++ database/factories/PostsFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Posts;
+use Faker\Generator as Faker;
+
+$factory->define(Posts::class, function (Faker $faker) {
+    return [
+        'title' => substr($faker->sentence(2), 0, -1),
+        'description' => $faker->paragraph,
+        'active' => 1
+    ];
+});
