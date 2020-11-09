8f7c428da4f11517eb5244b4967f82a9106eee5d
Ivan Batista <ivanatista@gmail.com>
2020-11-09 02:43:11 -0400

    Added TinyMCE rich text editor

--- resources/views/submit.blade.php
+++ resources/views/submit.blade.php
@@ -21,12 +21,27 @@
                 </div>
                 <div class="form-group">
                     <label for="description">Description</label>
-                    <textarea rows="5" class="form-control @error('description') is-invalid @enderror" id="description" name="description" placeholder="Description" value="{{old('description')}}"></textarea>
+                    <!-- <textarea rows="5" class="form-control @error('description') is-invalid @enderror" id="description" name="description" placeholder="Description" value="{{old('description')}}"></textarea> -->
                     @error('title')
                         <div class="invalid-feedback">{{$message}}</div>
                     @enderror
+                    <textarea class="description" name="description"></textarea>
                 </div>
                 <button type="sumbit" class="btn btn-primary">Submit</button>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+<script src="https://cdn.tiny.cloud/1/6y2l69omcikxmje86q0a723mashc908060ctjqri0it3qfhm/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+    tinymce.init({
+        selector: 'textarea',
+        plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+        toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+        toolbar_mode: 'floating',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'UNFPA',
+    });
+</script>
 @endsection
